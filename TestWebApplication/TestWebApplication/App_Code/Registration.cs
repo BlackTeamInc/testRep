e1e34d068c28bd2eab538ecc6e4e23e34008fa59
andrey <andrey23ru@gmail.com>
2015-10-18 09:25:13 +1000

    changes

---
+++ TestWebApplication/TestWebApplication/App_Code/Registration.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestWebApplication.App_Code
+{
+    public class Registration
+    {
+        public Registration()
+        {
+        }
+
+        public int CustomerID
+        {
+            get;
+            set;
+        }
+        public string ProductCode
+        {
+            get;
+            set;
+        }
+        public string RegistrationDate
+        {
+            get;
+            set;
+        }
+        
+
+
+    }
+}
+
