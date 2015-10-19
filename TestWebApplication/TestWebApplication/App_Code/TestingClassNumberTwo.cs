c45ae28e758addc5758e0727d34a120d9bc237b3
Carlos Poles <cadupoles1@gmail.com>
2015-10-19 11:12:58 +1000

    TestClass number two

---
+++ TestWebApplication/TestWebApplication/App_Code/TestingClassNumberTwo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestWebApplication.App_Code
+{
+    public class TestingClassNumberTwo
+    {
+    }
+}
